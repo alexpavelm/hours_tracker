c57828c3f7ebe53bd46a661af2f9afc5f3cb8d0f
alexpavelm <alexpavelm@gmail.com>
2021-04-04 00:16:12 +0300

    test commit 2

--- lib/resources/app_theme.dart
+++ lib/resources/app_theme.dart
@@ -8,7 +8,6 @@ enum AppTheme {
 final appThemeData = {
   AppTheme.LightMode : ThemeData(
     brightness: Brightness.light,
-    primaryColor: Colors.green
   ),
   AppTheme.DarkMode : ThemeData(
       brightness: Brightness.dark,
